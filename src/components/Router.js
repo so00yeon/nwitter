2bb8f638463d87d33679b34d73cf4a5b92b8ef08
soso <hazels201@gmail.com>
2023-03-31 00:36:44 +0900

    fix: isLoggedIn

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,7 +2,7 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
 
-const AppRouter = isLoggedIn => {
+const AppRouter = ({ isLoggedIn }) => {
     return (
         <Router>
             <Routes>
