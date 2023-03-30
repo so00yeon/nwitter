c48fc0e20618e4eabd7d87c72e2a584f1cf4fd5b
soso <hazels201@gmail.com>
2023-03-30 23:59:30 +0900

    add: auth service

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import AppRouter from "./Router";
+import AppRouter from "components/Router";
+import { authService } from "fBase";
 
 function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
     return (
         <>
             <AppRouter isLoggedIn={isLoggedIn} />
